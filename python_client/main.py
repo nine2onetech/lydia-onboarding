bfaa9da8d6d467eac7dbc190c6b775e08bc560b8
Lydia <lydia@socar.kr>
2024-11-11 11:47:37 +0900

    fix: socketio 를 aiohttp 를 이용한 비동기 서버로 변경

--- python_client/main.py
+++ python_client/main.py
@@ -1,113 +1,131 @@
+import asyncio
+import logging
 import json
 import threading
-import time
 
 import grpc
-import redis
+from redis.asyncio import Redis
 import socketio
-
+from aiohttp import web, ClientSession
 from protogen import bike_pb2_grpc, bike_pb2
 from google.protobuf import empty_pb2
 from google.protobuf.json_format import MessageToDict
 
 
-def get_redis_client() -> redis.Redis:
-    return redis.Redis(host="localhost", port=6379, decode_responses=True)
+# 로깅 설정
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(message)s", datefmt="%m/%d/%Y %I:%M:%S")
+logger = logging.getLogger(__name__)
 
 
 # Socket.IO 서버 생성
-sio = socketio.Server(logger=True, engineio_logger=True, async_mode="threading")
-app = socketio.WSGIApp(sio)
+sio = socketio.AsyncServer(async_mode="aiohttp", logger=False, engineio_logger=False)
+
+
+def get_redis_client() -> Redis:
+    """Redis 클라이언트 생성"""
+    return Redis(host="localhost", port=6379, decode_responses=True)
 
 
 # 연결 이벤트 핸들러
 @sio.event
-def connect(sid, environ):
-    print("클라이언트가 연결되었습니다:", sid)
+async def connect(sid, environ):
+    logger.info("클라이언트가 연결되었습니다: " + sid)
 
 
 # 메시지 이벤트 핸들러
 @sio.event
-def message(sid, data):
-    print("클라이언트로부터 메시지 수신:", data)
-    sio.emit("bike_return", "Return")
+async def message(sid, data):
+    # 클라이언트로부터 메시지 수신 (테스트용)
+    logger.debug("클라이언트로부터 메시지 수신: " + data)
+    await sio.emit("message", "Received: " + data)
 
 
 @sio.event
-def stn_list(sid, data):
-    print("클라이언트가 대여소 목록을 요청했습니다.")
+async def stn_list(sid, data):
+    logger.info("클라이언트가 대여소 목록을 요청했습니다.")
     r = get_redis_client()
     key = "stn_list"
-    if not r.exists(key):
-        print("Cache miss")
+    if not await r.exists(key):
+        logger.debug("Cache miss")
         with grpc.insecure_channel("localhost:9000") as channel:
             # Bike Service
             bike_stub = bike_pb2_grpc.BikeStub(channel)
             res: bike_pb2.StationList = bike_stub.GetStationList(empty_pb2.Empty())
-        data = res
+        data = [MessageToDict(station) for station in res.stations]
         # redis 에 캐시 저장
-        encoded = json.dumps(MessageToDict(data), ensure_ascii=False).encode("utf-8")
-        r.set(key, encoded)
+        encoded = json.dumps(data, ensure_ascii=False).encode("utf-8")
+        await r.set(key, encoded)
+        logger.debug("Cache set")
     else:
         # redis 에 이미 캐시가 있으면 캐시에서 응답
-        print("Cache hit")
-        data = r.get(key)
-    sio.emit("stn_list", data, room=sid)
+        logger.debug("Cache hit")
+        data = await r.get(key)
+    await sio.emit("stn_list", data)
+
+
+REQUEST_INTERVAL = 120  # 2분마다 요청
 
 
 # gRPC 데이터를 가져오는 함수
-def fetch_and_notify():
+async def fetch_and_notify_bike_rent_status():
     # gRPC 채널과 스텁 초기화
     r = get_redis_client()
-    with grpc.insecure_channel("localhost:9000") as channel:
-        stub = bike_pb2_grpc.BikeStub(channel)
+    async with ClientSession() as _:
+        with grpc.insecure_channel("localhost:9000") as channel:
+            stub = bike_pb2_grpc.BikeStub(channel)
 
-        while True:
-            # gRPC 서버 스트리밍 호출
-            try:
-                response_stream = stub.GetRealTimeStationStatus(empty_pb2.Empty())
-                for response in response_stream:
-                    key = response.stn_name
-                    value = response.parked_bike_cnt
+            while True:
+                # gRPC 서버 스트리밍 호출
+                try:
+                    response_stream = stub.GetRealTimeStationStatus(empty_pb2.Empty())
+                    for response in response_stream:
+                        key = response.stn_name
+                        value = response.parked_bike_cnt
 
-                    # Redis에서 이전 값 가져오기 + 없으면 설정하기
-                    previous_value = r.getset(key, value)
+                        # Redis에서 이전 값 가져오기 + 없으면 설정하기
+                        previous_value = await r.getset(key, value)
 
-                    # 값이 변경되었을 때만 업데이트 및 Socket.IO로 알림
-                    if previous_value is None:
-                        print(f"[REDIS] {key}: {value}")
-                    else:
-                        prv_int = int(previous_value)
-                        if prv_int == value:
-                            pass
-                        if prv_int > value:
-                            print(f"{key} 대여소의 자전거가 {prv_int - value} 대 빠졌습니다.")
-                            print(f"[REDIS] {key}: {prv_int} -> {value}")
-                            sio.start_background_task(
-                                sio.emit, "bike_rent", MessageToDict(response)
-                            )  # 웹 클라이언트에 알림 전송
-                        elif prv_int < value:
-                            print(f"{key} 대여소에 자전거가 {value - prv_int} 대 들어왔습니다.")
-                            print(f"[REDIS] {key}: {prv_int} -> {value}")
-                            sio.start_background_task(
-                                sio.emit, "bike_return", MessageToDict(response)
-                            )  # 웹 클라이언트에 알림 전송
+                        # 값이 변경되었을 때만 업데이트 및 Socket.IO로 알림
+                        if previous_value is None:
+                            logger.debug(f"[REDIS] {key}: {value}")
+                        else:
+                            prv_int = int(previous_value)
+                            if prv_int == value:
+                                pass
+                            elif prv_int > value:
+                                logger.info(f"{key} 대여소의 자전거가 {prv_int - value} 대 빠졌습니다.")
+                                logger.debug(f"[REDIS] {key}: {prv_int} -> {value}")
+                                await sio.start_background_task(
+                                    sio.emit, "bike_rent", MessageToDict(response)
+                                )  # 웹 클라이언트에 알림 전송
+                            else:
+                                logger.info(f"{key} 대여소에 자전거가 {value - prv_int} 대 들어왔습니다.")
+                                logger.debug(f"[REDIS] {key}: {prv_int} -> {value}")
+                                await sio.start_background_task(
+                                    sio.emit, "bike_return", MessageToDict(response)
+                                )  # 웹 클라이언트에 알림 전송
 
-                # 1분 대기 후 다시 요청
-                time.sleep(120)
+                    # 1분 대기 후 다시 요청
+                    await asyncio.sleep(REQUEST_INTERVAL)
 
-            except grpc.RpcError as e:
-                print("gRPC error:", e)
-                time.sleep(120)  # 오류 발생 시에도 1분 대기 후 재시도
+                except grpc.RpcError as e:
+                    print("gRPC error:", e)
+                    await asyncio.sleep(REQUEST_INTERVAL)  # 오류 발생 시에도 대기 후 재시도
 
 
-# gRPC 데이터 수신을 위한 스레드 시작
-grpc_thread = threading.Thread(target=fetch_and_notify, daemon=True)
-grpc_thread.start()
+def start_background_event_loop():
+    """서브 스레드에서 독립적인 이벤트 루프 생성 및 실행"""
+    loop = asyncio.new_event_loop()  # 서브 스레드에서 새 이벤트 루프 생성
+    asyncio.set_event_loop(loop)  # 현재 스레드의 이벤트 루프 설정
+    loop.run_until_complete(fetch_and_notify_bike_rent_status())
 
-# 실행할 때 웹 소켓 서버 시작
+
 if __name__ == "__main__":
-    # TODO: gunicorn 을 사용하도록 변경
-    # gunicorn --workers 1 --threads 2 --bind 127.0.0.1:8000 main:app
-    # eventlet.wsgi.server(eventlet.listen(("", 8000)), app)
-    pass
+    # 서버 시작 전에 백그라운드 스레드를 시작
+    thread = threading.Thread(target=start_background_event_loop)
+    thread.start()
+
+    # aiohttp 웹 서버 실행
+    app = web.Application(logger=logger)
+    sio.attach(app)
+    web.run_app(app)
